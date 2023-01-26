b759192f6a972c2e49a293dbe2f2762781dee12a
ibnurisk11 <ibnuriski28@gmail.com>
2023-01-26 16:46:14 +0700

    add

--- db/config.php
+++ db/config.php
@@ -1,4 +1,15 @@
-<?php 
-
+<?php
+/**
+ * using mysqli_connect for database connection
+ */
+ 
+$databaseHost = 'localhost';
+$databaseName = 'database';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
 
 ?>
